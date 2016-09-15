160564d70cd0426febb7e00e7f913eccf8447781
Remy Lalanne <remy@primcode.com>
2016-09-15 16:16:26 +0200

    search improved, sort order in hash

--- scripts/uiComponents/ui_search.js
+++ scripts/uiComponents/ui_search.js
@@ -1,5 +1,5 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
 		define([], factory);
 	} else {
@@ -7,286 +7,178 @@
 		root.compSearch = factory();
 	}
 }(this, function () {
-    loadCss('generic-ui/scripts/uiComponents/ui_search.css');
-    require(['domReady'], function (domReady) {
-        domReady(function () {
-            hashSearch(); 
-        });
-    });
 
-    /**
-    * HTML rendering methods for UI Components inside Layout
-    * require html container
-    * require damas.js
-    * call by process_hash function from main.js
-    */
+	loadCss('generic-ui/scripts/uiComponents/ui_search.css');
 
-    /**
-    * Listen specific component Hash
-    * 
-    */
-    function hashSearch() {
-        sortBy = '#parent';
-        order = 1;
+	window.addEventListener('hashchange', function(event){
+		hashSearch();
+	});
 
-        var hash = window.location.hash;
-        if (/search=/.test(hash) && !(/view=/.test(hash))) {
-            if (document.querySelector('.showAssetOverlay')){
-                var overlayDiv = document.querySelector('#assetOverlay');
-                overlayDiv.classList.remove('showAssetOverlay');
-                overlayDiv.classList.add('hideAssetOverlay');
-            }
-            var searchTerms = hash.replace(/#search=([^&]*).*/,'$1');
-//            var container = document.querySelector('#panelPrincipal');
+	var search_ui = { nbElements: 100 };
+
+	require(['domReady'], function (domReady) {
+		domReady(function () {
+			if (/#search=/.test(location.hash)){
+				// SEARCH RESULT MODE
+				hashSearch();
+			}
+			document.querySelector('#panelPrincipal').addEventListener('scroll', function(event){
+				if (/#search=/.test(location.hash)) {
+					var container = event.target;
+					if (container.scrollHeight - container.scrollTop === container.clientHeight){
+						doSearch();
+					}
+				}
+			});
+		});
+	});
+
+	function hashSearch() {
+		if (/#search=/.test(location.hash)){
 			var container = document.querySelector('#contents');
-            console.log(searchTerms);
-            container.innerHTML = '';
-            var searchAbout = document.createElement('div');
-            searchAbout.className = 'searchAbout';
-            searchAbout.innerHTML = 'Search results for "'+ searchTerms + '" :';
-            container.appendChild(searchAbout);
-            var tableBody = tableSearch(container, searchTerms);   
-            tableSearchSort(tableBody, searchTerms, sortBy, order);
-            var thead = document.getElementsByName(sortBy)[0];
-            sortIcon('up', thead);
-        }
-    };
+			container.innerHTML = '';
+			search_ui.offsetElements = 0;
+			draw(document.querySelector('#contents'));
+			doSearch();
+/*
+			var searchAbout = document.createElement('div');
+			searchAbout.className = 'searchAbout';
+			searchAbout.innerHTML = 'Search results for "'+ keys.search + '" :';
+			container.appendChild(searchAbout);
+*/
+		}
+	};
 
-    window.addEventListener('hashchange', function(event){
-        hashSearch();
-    });
+	function doSearch(){
+		var keys = getHash();
+		var sortBy = keys.sort || 'file';
+		var order = keys.order ? parseInt(keys.order) : 1
+		var sort = {};
+		sort[sortBy] = order;
+		damas.search_mongo({file: 'REGEX_'+keys.search}, sort, search_ui.nbElements, search_ui.offsetElements, function(res){
+			damas.read(res, function(assets){
+				var tableBody = document.querySelector('#contents tbody');
+				fill(tableBody, assets);
+				search_ui.offsetElements += search_ui.nbElements;
+			});
+		});
+	}
 
-    /**
-    * Generate Component : Search Results
-    * 
-    */
+	/**
+	* Input and button (for mobiles) elements for Search Component
+	* Localisation : Header
+	*/
 
-    function tableSearchSort(container, terms, sortBy, order){
-        var nbElements = 100;
-        offsetElements = 0;
-        sortBy = sortBy;
-        order = order;
+	function draw( container ) {
+		var searchInput = document.createElement('input');
+		searchInput.className = 'searchInput';
+		//searchInput.setAttribute('type', 'search');
+		searchInput.setAttribute('name', 'search');
+		searchInput.setAttribute('placeholder', 'Search');
+		searchInput.setAttribute('autocomplete', 'on');
+		searchInput.value = getHash().search;
+		searchInput.focus();
+		container.appendChild(searchInput);
+		searchInput.addEventListener('change', function(event) {
+			window.location.hash = '#search='+searchInput.value;
+		});
+		//searchInput.addEventListener('keyup', function(event) {
+			//if (searchInput.value.length > 0 && window.location.hash !== searchInput.value) {
+			//window.location.hash = '#search='+searchInput.value;
+			//}
+		//});
+		var table = document.createElement('table');
+		var thead = document.createElement('thead');
+		var th1 = document.createElement('th');
+		var th2 = document.createElement('th');
+		var th3 = document.createElement('th');
+		var tbody = document.createElement('tbody');
 
-        var sort = {};
-        sort[sortBy] = order;
+		table.className = 'search';
 
-        damas.search_mongo({'#parent': 'REGEX_'+terms+'.*'}, sort, nbElements, offsetElements, function(res){
-            damas.read(res, function(assets){
-                container.innerHTML = '';
-                tableSearchContent(container, assets);
-                offsetElements += nbElements;
-            });
-        });
-    };
+		th1.setAttribute('name', 'file');
+		th2.setAttribute('name', 'size');
+		th3.setAttribute('name', 'time');
 
-    var container = document.querySelector('#panelPrincipal');
-//	var container = document.querySelector('#contents');
-    container.addEventListener('scroll', function(event){
-        var hash = window.location.hash;
-        if (/search=/.test(hash)) {
-            event.preventDefault();
-            if (container.scrollHeight - container.scrollTop === container.clientHeight){
-                var sort = {};
-                sort[sortBy] = order;
-                var searchTerms = hash.replace(/#search=([^&]*).*/,'$1');
-                damas.search_mongo({'#parent': 'REGEX_'+searchTerms+'.*'}, sort, nbElements, offsetElements, function(res){
-                    damas.read(res, function(assets){
-                        var tableBody = document.querySelector('#contents tbody');
-                        tableSearchContent(tableBody, assets);
-                        offsetElements += nbElements;
-                    });
-                });
-            }
-        }
-    });
+		th1.innerHTML = 'file';
+		th2.innerHTML = 'size';
+		th3.innerHTML = 'time';
 
-    /**
-    * Input and button (for mobiles) elements for Search Component
-    * Localisation : Header
-    */
-    var searchForm = document.createElement('form');
-    searchForm.className = 'searchForm';
-    var searchInput = document.createElement('input');
-    searchInput.className = 'searchInput';
-//    searchInput.setAttribute('type', 'search');
-    searchInput.setAttribute('name', 'search');
-    searchInput.setAttribute('results', '10');
-    searchInput.setAttribute('placeholder', 'Search');
-    searchInput.setAttribute('autocomplete', 'on');
-    var searchReset = document.createElement('div');
-    searchReset.className = 'searchReset clickable';
-    searchReset.innerHTML = 'X';
-    searchReset.addEventListener('click', function(){
-        searchInput.value = '';
-        document.location.hash = '';
-    });
-    searchForm.appendChild(searchInput);
-    searchForm.appendChild(searchReset);
+		thead.appendChild(th1);
+		thead.appendChild(th2);
+		thead.appendChild(th3);
+		table.appendChild(thead);
+		table.appendChild(tbody);
+		container.appendChild(table);
 
-    var searchBt = document.createElement('a');
-    searchBt.className = 'searchBt clickable';
-    searchBt.innerHTML = 'search';
-    searchBt.setAttribute('title', 'Search');
+		var keys = getHash();
+		var sort = keys.sort || 'file';
+		var th = document.getElementsByName(sort)[0];
+		var icon = document.createElement('span');
+		var order = (keys.order)? keys.order: 1;
+		icon.innerHTML = (order==='1')? '&xutri;' : '&xdtri;';
+		th.innerHTML += ' ';
+		th.appendChild(icon);
 
-    document.getElementById('headerCentral').appendChild(searchForm);
-    document.getElementById('headerCentral').appendChild(searchBt);
-//	document.getElementById('menubar2').appendChild(searchForm);
-//    document.getElementById('menubar2').appendChild(searchBt);
+		for (var i=0; i < thead.children.length; i++){
+			thead.children[i].addEventListener('click', function(){
+				search_ui.offsetElements = 0;
+				var keys = getHash();
+				var order = (keys.order)? keys.order: 1;
+				window.location.hash = 'search='+keys.search+'&sort='+this.getAttribute('name')+'&order='+ (-order);
+			});
+		}
+		//return tbody;
+	}
 
-    searchForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        if (searchInput.value.length > 0 && window.location.hash !== searchInput.value) {
-            window.location.hash = '#search='+searchInput.value;
-        }
-        if (searchInput.value.length === 0) {
-            document.location.hash = '';
-        }
-    });
-    //searchForm.addEventListener('keyup', function(event) {
-    //    event.preventDefault();
-    //    if (searchInput.value.length > 0 && window.location.hash !== searchInput.value) {
-    //        
-    //    }
-    //});
-    searchBt.addEventListener('click', function(){
-        searchForm.style.display = 'inline-block';
-        searchBt.style.display = 'none';
-        searchReset.addEventListener('click', function(){
-            searchForm.style.display = 'none';
-            searchBt.style.display = 'block';
-        });
-    });
+	/**
+	 * Generate Table Content
+	 */
+	function fill(container, assets) {
+		for (var i=0; i<assets.length; i++) {
+			container.appendChild(tr(assets[i]));
+		}
+	}
 
-    /**
-    * Generate Table
-    * 
-    */
+	function tr(asset) {
+		var tr = document.createElement('tr');
+		var td1 = document.createElement('td');
+		var td2 = document.createElement('td');
+		var td3 = document.createElement('td');
+		var td4 = document.createElement('td');
+		td4.classList.add('buttons');
+		tr.setAttribute('title', JSON_tooltip(asset));
 
-    function tableSearch(container, terms) {
-        var table = document.createElement('table');
-        var thead = document.createElement('thead');
-        var th1 = document.createElement('th');
-        var th2 = document.createElement('th');
-        var th3 = document.createElement('th');
-        var tbody = document.createElement('tbody');
-		
-		table.className = 'log';
-		table.style.width = '100%';
+		var time = new Date(parseInt(asset.time));
+		var file = asset.file || asset['#parent'];
+		tr.file = file;
 
-        th1.setAttribute('name', 'time');
-        th2.setAttribute('name', '#parent');
-        th3.setAttribute('name', 'comment');
+		td2.innerHTML = human_size( asset.bytes || asset.size || asset.source_size);
+		td3.innerHTML = human_time(time);
+		var td4span = document.createElement('span');
+		td4span.setAttribute('title', 'delete');
+		td4span.classList.add('delete');
+		td4span.innerHTML = 'x';
+		td4.appendChild(td4span);
+		td4span.addEventListener('click', function(e){
+			if (confirm('Delete '+asset._id+' ?')) {
+				damas.delete(asset._id);
+			}
+		});
 
-        th1.innerHTML = 'time';
-        th2.innerHTML = 'file';
-        th3.innerHTML = 'comment';
+		td1.appendChild( human_filename_href(file) );
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		tr.appendChild(td4);
+		if (require.specified('ui_editor')){
+			tr.addEventListener('click', function(){
+				initEditor(asset);
+			});
+		}
+		return tr;
+	}
 
-        table.appendChild(thead);
-        thead.appendChild(th1);
-        thead.appendChild(th2);
-        thead.appendChild(th3);
-        table.appendChild(tbody);
-
-        for (var i=0; i < thead.children.length; i++){
-            thead.children[i].addEventListener('click', function(){
-                this.sorted ? (tableSearchSort(tbody, terms, this.getAttribute('name'), 1), sortIcon('up', this)) : (tableSearchSort(tbody, terms, this.getAttribute('name'), -1), sortIcon('down', this));
-                this.sorted = !this.sorted;
-            });
-        }
-
-        container.appendChild(table);
-        return tbody;
-    }
-
-    function sortIcon(status, th){
-        if (document.querySelector('.sortIcon'))
-            document.querySelector('.sortIcon').remove();
-        var icon = document.createElement('span');
-        icon.className = 'sortIcon';
-        if (status === 'up'){
-            icon.innerHTML = '&xutri;';
-            th.appendChild(icon);
-        }
-        if (status === 'down'){
-            icon.innerHTML = '&xdtri;';
-            th.appendChild(icon);
-        }
-    }
-
-    /**
-    * Generate Table Content
-    * 
-    */
-    function tableSearchContent(container, assets) {
-        for (var i=0; i<assets.length; i++) {
-            var tr = document.createElement('tr');
-            var td1 = document.createElement('td');
-            var td2 = document.createElement('td');
-            var td3 = document.createElement('td');
-
-            td2.setAttribute('title', JSON_tooltip(assets[i]));
-            
-			var time = new Date(parseInt(assets[i].time));
-            var file = assets[i].file || assets[i]['#parent'];
-            tr.file = file;
-
-			td1.innerHTML = ('00'+time.getDate()).slice(-2)+'/'+('00'+time.getMonth()).slice(-2)+' '+('00'+time.getHours()).slice(-2)+':'+('00'+time.getMinutes()).slice(-2)+':'+('00'+time.getSeconds()).slice(-2);
-			td3.innerHTML = '&lt;'+assets[i].author+'&gt; '+assets[i].comment;
-            
-            var path = document.createElement('span');
-            path.className = 'nomobile';
-            var filename = document.createElement('span');
-            if (file){
-                path.innerHTML = file.split('/').slice(0,-1).join('/')+'/';
-                filename.innerHTML = file.split('/').pop();
-            }
-			td2.setAttribute('title', JSON_tooltip(assets[i]));
-            td2.appendChild(path);
-            td2.appendChild(filename);
-
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-            container.appendChild(tr);
-            
-            if (require.specified('ui_overlay')){
-                td2.className ='clickable';
-                td2.addEventListener('click', function(){
-                    addHash('view='+this.parentNode.file);
-                    if (document.querySelector('tr.selected')){
-                        document.querySelector('tr.selected').classList.remove('selected');
-                    }
-                    this.parentNode.className = 'selected';
-                });
-            }
-
-            if (require.specified('ui_editor')){
-                tr.id = asset._id;
-                var tdEdit = document.createElement('td');
-                tdEdit.className = 'fa fa-pencil clickable';
-                tr.appendChild(tdEdit);
-                tdEdit.addEventListener('click', function(){
-//                    var currentHash = window.location.hash;
-//                    if (/edit=/.test(currentHash)) {
-//                        var splitHash = currentHash.split('&');
-//                        splitHash.pop();
-//                        var arr = [];
-//                        arr.push(splitHash);
-//                        arr.push('edit='+this.parentNode.file);
-//                        window.location.hash = arr.join('&');
-//                    }
-//                    else {
-//                        addHash('edit='+this.parentNode.file);
-//                    }
-                    
-//                    window.location.hash = 'edit='+this.parentNode.file;
-                    initEditor(this.parentNode.file);
-                    if (document.querySelector('tr.selected')){
-                        document.querySelector('tr.selected').classList.remove('selected');
-                    }
-                    this.parentNode.className = 'selected';
-                });
-            }
-        }
-    }
 }));
+
+
